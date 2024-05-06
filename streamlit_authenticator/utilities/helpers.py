324699844cdef98f6096b7aa5b7c20fda0e5b721
Mohammad Khorasani <khorasani.mohammad@gmail.com>
2024-05-06 16:31:17 +0300

    Update helpers.py

--- streamlit_authenticator/utilities/helpers.py
+++ streamlit_authenticator/utilities/helpers.py
@@ -19,6 +19,21 @@ class Helpers:
     def __init__(self):
         pass
     @classmethod
+    def generate_captcha(cls) -> tuple:
+        """
+        Generates a captcha image.
+
+        Returns
+        -------
+        int
+            The randomly generated four digit captcha.
+        ImageCaptcha
+            The randomly generated captcha object.
+        """
+        image = ImageCaptcha(width=120, height=75)
+        random_digit = random.choices(string.digits, k=4)
+        return random_digit, image.generate(random_digit)
+    @classmethod
     def generate_random_pw(cls, length: int=16) -> str:
         """
         Generates a random password.
@@ -34,19 +49,3 @@ class Helpers:
         """
         letters = string.ascii_letters + string.digits
         return ''.join(random.choice(letters) for i in range(length)).replace(' ','')
-    @classmethod
-    def generate_captcha(cls) -> tuple:
-        """
-        Generates a captcha image.
-
-        Returns
-        -------
-        int
-            The randomly generated four digit captcha.
-        ImageCaptcha
-            The randomly generated captcha object.
-        """
-        image = ImageCaptcha(width=120, height=75)
-        random_digit = random.choices(string.digits, k=4)
-        return random_digit, image.generate(random_digit)
-    
